28dab2eaaa1c416716d10a201e60edb909e9f579
Adam Barrell <a.j.barrell@gmail.com>
2015-04-06 13:44:48 +0100

    Added nohup to mvn test command to prevent stoppage on logout.

--- start-tests.sh
+++ start-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-i386/jre/lib/i386/jli/ 
-cd /var/www/sizeguide.adambarrell.co.uk/web && mvn test &
+cd /var/www/sizeguide.adambarrell.co.uk/web && nohup mvn test &
