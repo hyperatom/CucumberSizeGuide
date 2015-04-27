9dc6ca91d7a969fe89386c9827c67b10976ee1b4
Adam Barrell <a.j.barrell@gmail.com>
2015-04-27 11:46:55 +0100

    Adjusted path to application root.

--- start-tests.sh
+++ start-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-i386/jre/lib/i386/jli/ 
-cd /var/www/sizeguide.adambarrell.co.uk/web && nohup mvn test &
+cd /var/www/sizeguide.adambarrell.co.uk/web/app && nohup mvn test &
