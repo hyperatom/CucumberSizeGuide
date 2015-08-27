3dc671bf659a03b843c4ae1098020e6af0350db5
Adam Barrell <a.j.barrell@gmail.com>
2015-08-27 14:38:24 +0100

    Finalised playbook for server provisioning.

--- start-tests.sh
+++ start-tests.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-i386/jre/lib/i386/jli/ 
-cd /var/www/sizeguide.adambarrell.co.uk/web/app && nohup mvn test &
+cd /var/www/SizeGuideTester && nohup mvn test &
